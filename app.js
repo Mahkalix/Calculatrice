d470bfb801d7c976116c3dea279a6a8e6422b8de
Maxence Badin Léger <maxencelger@gmail.com>
2023-11-17 17:04:18 +0100

    feat: addeventlsitener js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // DOM
 
 const touches = document.querySelectorAll(".button");
-
 console.log(touches);
+
+// Convert NodeList to Array
+const touchesArray = Array.from(touches);
+// or const touchesArray = [...touches];
+
+const listKeycode = touchesArray.map((touche) => touche.dataset.key);
+console.log(listKeycode);
+
+document.addEventListener("keydown", (e) => {
+  const valeur = e.keyCode.toString();
+  //   console.log(valeur, typeof valeur);
+});
+
+document.addEventListener("click", (e) => {
+  const valeur = e.target.dataset.key;
+  //   console.log(valeur, typeof valeur);
+});
