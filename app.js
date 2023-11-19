d4a7ba167decc307df96bed62e999e64917fef68
Maxence Badin Léger <maxencelger@gmail.com>
2023-11-19 19:14:35 +0100

    feat: eval()
    trouver une autre solution pour la suite

--- app.js
+++ app.js
@@ -3,19 +3,49 @@
 const touches = document.querySelectorAll(".button");
 console.log(touches);
 
+const ecran = document.querySelector(".ecran");
+
 // Convert NodeList to Array
 const touchesArray = Array.from(touches);
 // or const touchesArray = [...touches];
 
 const listKeycode = touchesArray.map((touche) => touche.dataset.key);
-console.log(listKeycode);
+// console.log(listKeycode);
 
 document.addEventListener("keydown", (e) => {
   const valeur = e.keyCode.toString();
   //   console.log(valeur, typeof valeur);
+  calculer(valeur);
 });
 
 document.addEventListener("click", (e) => {
   const valeur = e.target.dataset.key;
   //   console.log(valeur, typeof valeur);
+  calculer(valeur);
+});
+
+const calculer = (valeur) => {
+  if (!isNaN(parseInt(valeur))) {
+    console.log("yes");
+  }
+  if (listKeycode.includes(valeur)) {
+    switch (valeur) {
+      case "8":
+        ecran.textContent = "";
+        break;
+      case "13":
+        const calcul = eval(ecran.textContent);
+        ecran.textContent = calcul;
+        break;
+      default:
+        const indexKeycode = listKeycode.indexOf(valeur);
+        const touche = touches[indexKeycode];
+        ecran.textContent += touche.innerHTML;
+    }
+  }
+};
+
+window.addEventListener("error", (e) => {
+  alert("Une erreur est survenue dans votre calcul : " + e.message);
+  ecran.textContent = "";
 });
